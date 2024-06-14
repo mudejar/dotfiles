5188f636b36cf6a1d7c6377c08a359c1efe2c8ee
mudejar <baljishi@proton.me>
2024-06-14 16:55:45 -0700

    Added code to install yay package manager if not found in an arch installation.

--- installers/arch_linux/installer.sh
+++ installers/arch_linux/installer.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/zsh
 
+cd $HOME
+
 # Check if yay is installed
 if! [ command -v yay &> /dev/null ] then
-    echo "yay could not be found. Please install yay."
-    exit 1
+  pacman -Sy --needed git base-devel
+  git clone https://aur.archlinux.org/yay.git
+  cd yay
+  makepkg -si
 fi
 
 echo "Downloading and installing packages listed in packages.txt file..."
 yay -S --needed - < packages.txt 
 
 echo "Installing polybar themes..."
+cd $HOME
 git clone --depth=1 https://github.com/adi1090x/polybar-themes.git
 cd polybar-themes
 chmod +x setup.sh
