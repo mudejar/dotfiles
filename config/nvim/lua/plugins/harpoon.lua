b922fbd74c71b4c09804d683541329c91a1c3f5e
Bader Aljishi <bader.aljishi@appdynamics.com>
2024-08-28 10:12:36 -0700

    Added mappings for navigating harpoon queue, but only next and previous functions.

--- config/nvim/lua/plugins/harpoon.lua
+++ config/nvim/lua/plugins/harpoon.lua
@@ -19,43 +19,43 @@ return {
       end,
       { desc = "Toggle the harpoon list of files"}
     },
+    -- {
+    --   "<C-j>",
+    --   function()
+    --     require("harpoon"):list():select(1)
+    --   end,
+    --   { desc = "Select file in 1 position" }
+    -- },
+    -- {
+    --   "<C-k>",
+    --   function()
+    --     require("harpoon"):list():select(2)
+    --   end,
+    --   { desc = "Select file in 2 position" }
+    -- },
+    -- {
+    --   "<C-l>",
+    --   function()
+    --     require("harpoon"):list():select(3)
+    --   end,
+    --   { desc = "Select file in 3 position" }
+    -- },
+    -- {
+    --   "<C>;",
+    --   function()
+    --     require("harpoon"):list():select(4)
+    --   end,
+    --   { desc = "Select file in 4 position" }
+    -- }
     {
-      "<C-u>",
-      function()
-        require("harpoon"):list():select(1)
-      end,
-      { desc = "Select file in 1 position" }
-    },
-    {
-      "<C-i>",
-      function()
-        require("harpoon"):list():select(2)
-      end,
-      { desc = "Select file in 2 position" }
-    },
-    {
-      "<C-o>",
-      function()
-        require("harpoon"):list():select(3)
-      end,
-      { desc = "Select file in 3 position" }
-    },
-    {
-      "<C-p>",
-      function()
-        require("harpoon"):list():select(4)
-      end,
-      { desc = "Select file in 4 position" }
-    },
-    {
-      "<C-1-p>",
+      "<C-S-p>",
       function()
         require("harpoon"):list():prev()
       end,
       { desc = "Go to previous file in Harpoon list" }
     },
     {
-      "<C-1-n>",
+      "<C-S-n>",
       function()
         require("harpoon"):list():next()
       end,
