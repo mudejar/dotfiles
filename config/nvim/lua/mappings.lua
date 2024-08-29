da111c327c4be9c5305d275ddae4f2a398082003
Bader Aljishi <bader.aljishi@appdynamics.com>
2024-08-29 10:38:37 -0700

    Modified descriptions of window related key mappings so that they show up on the NvChad cheatsheet properly.

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -8,12 +8,12 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- More keybinds taken from https://neovim.io/doc/user/vimindex.html
-map("n", "<C-q>", "<C-W>q", { desc = "Quit current window" })
-map("n", "<C-r>", "<C-W>r", { desc = "Rotate windows downwards N times" })
-map("n", "<C-s>", "<C-W>s", { desc = "Split the current window into two parts, new Window N lines high" })
-map("n", "<C-v>", "<C-W>v", { desc = "Split the current window vertically, new window N columns wide"})
+map("n", "<C-q>", "<C-W>q", { desc = "Window Quit current window" })
+map("n", "<C-r>", "<C-W>r", { desc = "Window Rotate windows downwards N times" })
+map("n", "<C-s>", "<C-W>s", { desc = "Window Split the current window into two parts, new Window N lines high" })
+map("n", "<C-v>", "<C-W>v", { desc = "Window Split the current window vertically, new window N columns wide"})
 
 -- Keybinds to make common tasks easier to call
-map("n", "<C-g>", "1<C-G>", { desc = "Show the full filepath of the file currently opened" })
+map("n", "<C-g>", "1<C-G>", { desc = "File Show the full filepath of the file currently opened" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
