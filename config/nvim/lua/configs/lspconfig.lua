48b3d8e4e5b152c3b91dcc7138624dc8edfba336
Bader Aljishi <baljishi@splunk.com>
2026-02-20 09:57:16 -0800

    Removed statements requiring 'lspconfig' which were triggering a deprecation warning on Neovim 0.11+

--- config/nvim/lua/configs/lspconfig.lua
+++ config/nvim/lua/configs/lspconfig.lua
@@ -1,40 +1,19 @@
--- EXAMPLE 
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-local on_init = require("nvchad.configs.lspconfig").on_init
-local capabilities = require("nvchad.configs.lspconfig").capabilities
-
-local lspconfig = require "lspconfig"
+-- Servers with default config (capabilities, on_init, on_attach are set
+-- globally by NvChad's defaults() via vim.lsp.config("*", ...))
 local servers = { "html", "cssls", "vimls", "bashls", "jsonls", "jdtls" }
-local python_servers = { "pyright", "ruff" }
-local util = require "lspconfig/util"
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-  }
-end
+vim.lsp.enable(servers)
 
 -- Python
-for _, lsp in ipairs(python_servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-    filetypes = { "python" },
-  }
-end
+vim.lsp.config("pyright", { filetypes = { "python" } })
+vim.lsp.config("ruff", { filetypes = { "python" } })
+vim.lsp.enable { "pyright", "ruff" }
 
 -- Golang
-lspconfig.gopls.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-  cmd = {'gopls'},
+vim.lsp.config("gopls", {
+  cmd = { "gopls" },
   filetypes = { "go", "gomod", "gowork", "gotmpl" },
-  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  root_markers = { "go.work", "go.mod", ".git" },
   settings = {
     gopls = {
       completeUnimported = true,
@@ -44,11 +23,5 @@ lspconfig.gopls.setup {
       },
     },
   },
-}
-
--- typescript
--- lspconfig.tsserver.setup {
---   on_attach = on_attach,
---   on_init = on_init,
---   capabilities = capabilities,
--- }
+})
+vim.lsp.enable "gopls"
